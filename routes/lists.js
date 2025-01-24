ab91a0b0f6799382d6d59546af0c72730d1c7687
esteep <esteep@Steepest.local>
2025-01-23 18:19:54 -0800

    Fix List model import case sensitivity

--- routes/lists.js
+++ routes/lists.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const List = require('../models/List');
+const List = require('../models/list');
 const Word = require('../models/Word');
 
 // Get user's lists

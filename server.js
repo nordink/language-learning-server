5a5270da80a7bdf3c00fd725240010b1086e3209
esteep <esteep@Steepest.local>
2025-01-31 14:35:42 -0800

    update CORS for list import

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const app = express();
 const corsOptions = {
   origin: [
     'https://aquamarine-shortbread-a36146.netlify.app',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://steepy.org'  
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
